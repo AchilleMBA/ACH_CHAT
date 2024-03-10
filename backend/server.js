ae5fe832c28142bf9fb883def3ce0dbd0f90ba5c
Achille Maboa <achille.maboa@icloud.com>
2024-03-10 16:49:46 +0100

    models

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+import express from "express";
+import dotenv from "dotenv" ;
+
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app =express();
+const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
